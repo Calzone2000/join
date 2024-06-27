0ebd633967fcc0bb969089be61ae96a4fbcffb9c
Lukasz <lukasz.barszczewski@yahoo.com>
2024-06-27 15:23:29 +0200

    form validation works correctly

--- scripts/login.js
+++ scripts/login.js
@@ -7,7 +7,15 @@ async function createNewUser() {
     const confirmPassword = document.getElementById('confirm-password').value;
     const acceptPrivacyPolicyCheckbox = document.getElementById('acceptPrivacyPolicyCheckbox');
 
-    // if (submitConditions(name, email, password, confirmPassword, acceptPrivacyPolicyCheckbox)) {
+    if (password !== confirmPassword) {
+        document.getElementById('password').style.border = 'solid 2px rgb(252, 3, 3)';
+        document.getElementById('confirm-password').style.border = 'solid 2px rgb(252, 3, 3)';
+        return;
+    } else {
+        document.getElementById('password').style.border = '';
+        document.getElementById('confirm-password').style.border = '';
+    }
+
 
     let data = {
         name: name,
@@ -23,49 +31,6 @@ async function createNewUser() {
         alert('There was an error creating the user.');
     }
 }
-
-
-
-// boolean werte: 
-
-// function submitConditions(name, email, password, confirmPassword, acceptPrivacyPolicyCheckbox) {
-//     let validName = false;
-//     let validEmail = false;
-
-//     if (name) {
-//         validName = true;
-//     } else {
-//         document.getElementById('name').style.border = 'solid 2px rgb(255, 4, 4)';
-//         return;
-//     }
-
-//     if (email) {
-//         validEmail = true;
-//     } else {
-//         document.getElementById('email').style.border = 'solid 2px rgb(255, 4, 4)';
-//         return;
-//     }
-
-//     if (!validName && !validEmail) {
-//         document.getElementById('name').style.border = 'solid 2px rgb(255, 4, 4)';
-//         document.getElementById('email').style.border = 'solid 2px rgb(255, 4, 4)';
-//         return false;
-//     }
-
-//     if (password !== confirmPassword) {
-//         document.getElementById('password').style.border = 'solid 2px rgb(255, 4, 4)';
-//         document.getElementById('confirm-password').style.border = 'solid 2px rgb(255, 4, 4)';
-//         return;
-//     }
-
-//     if (!acceptPrivacyPolicyCheckbox.checked) {
-//         alert("Please accept the Privacy Policy!")
-//         return;
-//     }
-
-//     return true;
-// }
-
 
 async function postNewUser(path = "", data = {}) {
     let response = await fetch(BASE_URL + path + '.json', {
