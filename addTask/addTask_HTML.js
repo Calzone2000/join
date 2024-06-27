b64b202446fc6aee0607418a2ee69a706e13bcac
Lea Cotar <lea.cotar@gmx.ch>
2024-06-27 11:52:43 +0200

    new functions

--- addTask/addTask_HTML.js
+++ addTask/addTask_HTML.js
@@ -1,19 +1,37 @@
+let currentState = [
+  {
+    toDo: [],
+  },
+
+  {
+    inProgress: [],
+  },
+
+  {
+    awaitFeedback: [],
+  },
+
+  {
+    done: [],
+  },
+];
+
 function renderGetSubtasks(i, element) {
   return /* html */ `
     <div class="showTaskSubtaskEditBTN" id="showTaskSubtaskEditBTN${i}">
-        <input type="text" id="showTaskSubtaskEditInput${i}">
-        <div>
-            <img src="../assets/img/delete.svg" alt="" onclick="delNewTask(${i})">
-            <img src="../assets/img/check.svg" alt="" onclick="editNewTask(${i})" >
+        <input type="text" id="showTaskSubtaskEditInput${i}" value="${element}">
+        <div class="editingDeleting">
+            <img src="../assets/img/delete.svg" alt="" onclick="delNewTask(${i})" class="delSubtask">
+            <img src="../assets/img/check.svg" alt="" onclick="saveEditNewTask(${i})" class="subtaskTakeover">
         </div>
     </div>
 
     <div class="editSubtasks_delEdit">
         <div class="showTask"><li>${element}</li></div>
-        <div class="showTask_editSubtasks_delEditBTN">
-            <img class="imgHoverBTN" src="./assets/img/edit.svg" onclick="showEditNewSubTask(${i})">
+        <div class="showSubTask">
+            <img class="imgHoverBTN" src="../assets/img/edit.svg" onclick="EditNewSubTask(${i})">
             <div class="crossLine"></div>
-            <img class="imgHoverBTN" src="./assets/img/delete.svg"  onclick="delNewTask(${i})">
+            <img class="imgHoverBTN" src="../assets/img/delete.svg"  onclick="delNewTask(${i})">
         </div>
     </div>
     `;
